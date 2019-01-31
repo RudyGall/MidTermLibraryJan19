9ba2a66ea26e9163deb1e4f825d26d44feec2408
Rudy Gall <rudyegall@gmail.com>
2019-01-31 13:36:18 -0500

    Initial commit remake, need to clean up code

---
+++ MidtermLibraryToUpload/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermLibraryToUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // populate Library based on file
+            LoadSave.InitialStream();
+
+            Console.WriteLine("Success!");
+            System.Threading.Thread.Sleep(2000);
+
+            // loop through Menu() for as long as needed
+            do
+            {
+                LibraryActions.Menu();
+            }
+            while (LibraryActions.userSelectedQuit);
+
+            // Write all changes to libraryFile
+            LoadSave.UpdateLibraryFile();
+            Console.WriteLine("Library file has been updated! Press any key to close.");
+
+            // end
+            Console.ReadKey();
+        }
+
+        public static List<Book> Library = new List<Book>();
+
+
+
+    }
+}
